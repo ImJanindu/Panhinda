27c64cbc244b75087ce74b70718dda71002f6e9c
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2025-07-10 21:38:51 +0530

    Update panhinda.py

--- panhinda.py
+++ panhinda.py
@@ -8,3 +8,7 @@ def make_shell_context():
     from app.articles.models import Article, Category, SubCategory
 
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Article': Article, 'Category': Category, 'SubCategory': SubCategory}
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)

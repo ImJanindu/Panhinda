2be386177a933a0082e9ff93f80471997ebe4cd9
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2025-07-10 22:05:55 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -42,3 +42,6 @@ app.register_blueprint(profile.bp)
 # Register custom Jinja filter
 from app.utils.func import utc_to_local
 app.jinja_env.filters['utc_to_local'] = utc_to_local
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
